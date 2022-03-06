652883a897c3eabf1240e3d78ff4e944842586ed
mustafa <mltm0204@gmail.com>
2022-03-06 11:26:07 +0300

    ...

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,57 +1,70 @@
-/** @format */
 
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
 import { Stack, Tab, Tabs } from "@mui/material";
 import PropTypes from "prop-types";
-import './Modal.css'
+import "./Modal.css";
 import Options from "../options/Options";
-import NextModal from "../nextModal/NextModal"
+import NextModal from "../nextModal/NextModal";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role='tabpanel'
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}>
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
+const steps = ["TAKE ACTION", "SELECT ACTION"];
 
 export default function Modals() {
   const [open, setOpen] = React.useState(false);
+  const [activeStep, setActiveStep] = React.useState(0);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const [value, setValue] = React.useState(0);
+  
+  // const [skipped, setSkipped] = React.useState(new Set());
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  // const isStepOptional = (step) => {
+  //   return step === 1;
+  // };
+
+  // const isStepSkipped = (step) => {
+  //   return skipped.has(step);
+  // };
+
+  const handleNext = () => {
+    // let newSkipped = skipped;
+    // if (isStepSkipped(activeStep)) {
+    //   newSkipped = new Set(newSkipped.values());
+    //   newSkipped.delete(activeStep);
+    // }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    // setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  // const handleSkip = () => {
+  //   if (!isStepOptional(activeStep)) {
+  //     // You probably want to guard against something like this,
+  //     // it should never occur unless someone's actively trying to break something.
+  //     throw new Error("You can't skip a step that isn't optional.");
+  //   }
+
+  // setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  // setSkipped((prevSkipped) => {
+  //   const newSkipped = new Set(prevSkipped.values());
+  //   newSkipped.add(activeStep);
+  //   return newSkipped;
+  // });
+  //}
+
+  const handleReset = () => {
+    setActiveStep(0);
   };
 
   return (
@@ -61,7 +74,7 @@ export default function Modals() {
           NO ACTION NEEDED
         </Button>
         <Button
-          onClick={handleOpen}
+          onClick={()=>handleOpen()}
           variant='contained'
           color='success'
           className='take-action'>
@@ -71,34 +84,66 @@ export default function Modals() {
       <Modal
         keepMounted
         open={open}
-        onClose={handleClose}
+        onClose={()=>handleClose()}
         aria-labelledby='keep-mounted-modal-title'
         aria-describedby='keep-mounted-modal-description'>
-        <Box sx={{ width: "45%" }} className='box-modal'>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              >
-              <Tab label='SELECT ACTION' {...a11yProps(0)} />
-              <Tab label='TAKE ACTION'{...a11yProps(1)} />
-            </Tabs>
-          </Box>
-          <TabPanel value={value} index={0}>
-                <Options/>
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            Item Two
-          </TabPanel>
-          <Tabs onChange={handleChange} value={value}>
-            <Button 
-               
-              variant='contained'
-              color='success'
-              {...a11yProps(1)} 
-              >NEXT
-            </Button>
-          </Tabs>
+
+        <Box sx={{ width: "50%" }} className='box-modal'>
+          <Stepper activeStep={activeStep}>
+            {steps.map((label, index) => {
+              const stepProps = {};
+              const labelProps = {};
+
+              // if (isStepSkipped(index)) {
+              //   stepProps.completed = false;
+              // }
+              return (
+                <Step key={index} {...stepProps}>
+                  <StepLabel {...labelProps}>{label}</StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
+          {activeStep === steps.length ? (
+            <React.Fragment>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                All steps completed - you&apos;re finished
+              </Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Box sx={{ flex: "1 1 auto" }} />
+                <Button onClick={()=>handleReset()}>Reset</Button>
+              </Box>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                {activeStep + 1 === 1 && <Options/>}
+              </Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Button
+                  variant='contained'
+                  color='secondary'
+                  disabled={activeStep === 0}
+                  onClick={()=>handleBack()}
+                  sx={{ mr: 1 }}>
+                  Back
+                </Button>
+                <Box sx={{ flex: "1 1 auto" }} />
+                {/* {isStepOptional(activeStep) && (
+              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                Skip
+              </Button>
+            )} */}
+
+                <Button
+                  onClick={()=>handleNext()}
+                  variant='contained'
+                  color='success'>
+                  {activeStep === steps.length - 1 ? "Take an action" : "Next"}
+                </Button>
+              </Box>
+            </React.Fragment>
+          )}
         </Box>
       </Modal>
     </div>
