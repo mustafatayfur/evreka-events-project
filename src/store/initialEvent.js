e149d5de4db41090bbad11b7d44050632c390768
mustafa <mltm0204@gmail.com>
2022-03-06 09:13:21 +0300

    ...

---
+++ src/store/initialEvent.js
@@ -0,0 +1,84 @@
+let initialEvent = {
+    media: [
+      {
+        url: 'https://media.evreka.co/1588172618.mp3',
+        type: 'audio',
+      },
+    ],
+    actions: [
+      {
+        comment: '',
+        task_id: '',
+        title: 'Aksiyon Gerekmiyor',
+        user: '',
+        date: '',
+        action_taken: false,
+        action_id: 0,
+      },
+      {
+        comment: '',
+        task_id: '',
+        title: 'Çözüm Bildir',
+        user: '',
+        date: '',
+        action_taken: false,
+        action_id: 1,
+      },
+    ],
+    extras: {
+      new: false,
+      vehicle_id: 156,
+      driver_id: '03085',
+    },
+    details: [
+      {
+        format: 'date',
+        value: '2020-04-29T15:03:37.406Z',
+        title: 'Tarih',
+      },
+      {
+        format: 'incident_type',
+        value: 'Breakdown',
+        title: 'Tip',
+      },
+      {
+        format: 'string',
+        link: '/routing/operations/1',
+        value: 'Plansız Rota 10:51',
+        title: 'Rota İsmi',
+      },
+      {
+        format: 'string',
+        value: 'dsfvbnv ',
+        title: 'Kategori',
+      },
+      {
+        format: 'string',
+        value: '-',
+        title: 'Aksiyon',
+      },
+      {
+        format: 'string',
+        value: '-',
+        title: 'Kalan Görev(ler)',
+      },
+      {
+        format: 'vehicle',
+        value: '06 ADN 61',
+        title: 'Araç',
+      },
+      {
+        format: 'string',
+        value: 'İmren KARALAR',
+        title: 'Şoför',
+      },
+    ],
+    type: 'incident',
+    id: 13408,
+    location: {
+      latitude: 39.9229025,
+      type: 'single_point',
+      longitude: 32.8419197,
+    },
+  }
+  export default initialEvent
